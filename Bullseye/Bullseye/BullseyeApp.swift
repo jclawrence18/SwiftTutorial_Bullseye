ef13de170705c683524f1ebabebef4e08e593511
Caleb Lawrence <caleblawrence@icloud.com>
2021-03-01 21:24:57 -0600

    Completed Part 1: Lessons 1-5

---
+++ Bullseye/Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Caleb Lawrence on 3/1/21.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
